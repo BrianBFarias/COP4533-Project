bc3ce10f03f8237e561a47d7a12520ae3dc0a12d
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-10-11 11:42:25 -0400

    made O(n) complexity

--- Program2.java
+++ Program2.java
@@ -12,41 +12,98 @@ class Program2{
     * @return Result object containing the number of platforms, total height of the paintings and the number of paintings on each platform
     */
     private static Result program2(int n, int w, int[] heights, int[] widths) {
-        int[] dp = new int[n + 1];
-        int[] platforms = new int[n + 1];
-        int[] lastPlatform = new int[n];
 
-        for (int i = 1; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-            int currentWidth = 0;
-            int currentMaxHeight = 0;
+        int totalHeight = 0;
+        int currentWidth = 0;
+        int currentMaxHeight = 0;
+        int localMin = 0;
 
-            for (int j = i; j > 0; j--) {
-                currentWidth += widths[j - 1];
+        int[] numPaintingsOnPlatformFirstPhase = new int[n];
+        int[] numPaintingsOnPlatformSecondPhase = new int[n]; 
+        int firstPhasePlatformCount = 0;
+        int secondPhasePlatformCount = 0;
 
-                if (currentWidth > w) break;
-                
-                currentMaxHeight = Math.max(currentMaxHeight, heights[j - 1]);
-                
-                if (dp[i] > dp[j - 1] + currentMaxHeight) {
-                    dp[i] = dp[j - 1] + currentMaxHeight;
-                    platforms[i] = platforms[j - 1] + 1;
-                    lastPlatform[i - 1] = j - 1;
-                }
+        int paintingsOnCurrentPlatform = 0;
+        boolean localMinProcessed = false;
+
+        int i = 0;
+
+        // Front of Local Min
+        while (heights[i] > heights[i + 1]) {
+            if (currentWidth + widths[i] <= w) {
+                currentWidth += widths[i];
+                currentMaxHeight = Math.max(currentMaxHeight, heights[i]);
+                paintingsOnCurrentPlatform++;
+
+            } else {
+                totalHeight += currentMaxHeight;
+                numPaintingsOnPlatformFirstPhase[firstPhasePlatformCount] = paintingsOnCurrentPlatform;
+                firstPhasePlatformCount++;
+
+                currentWidth = widths[i];
+                currentMaxHeight = heights[i];
+                paintingsOnCurrentPlatform = 1;
             }
+            i++;
         }
 
-        int numPlatforms = platforms[n];
-        int[] numPaintingsOnPlatform = new int[numPlatforms];
-        int currentPainting = n;
-        
-        for (int i = numPlatforms - 1; i >= 0; i--) {
-            numPaintingsOnPlatform[i] = currentPainting - lastPlatform[currentPainting - 1];
-            currentPainting = lastPlatform[currentPainting - 1];
+        localMin = i;
+        if (currentWidth + widths[i] <= w) {
+            currentWidth += widths[i];
+            currentMaxHeight = Math.max(currentMaxHeight, heights[i]);
+            paintingsOnCurrentPlatform++;
+            localMinProcessed = true;
         }
 
-        return new Result(numPlatforms, dp[n], numPaintingsOnPlatform);
+        if (paintingsOnCurrentPlatform > 0) {
+            totalHeight += currentMaxHeight;
+            numPaintingsOnPlatformFirstPhase[firstPhasePlatformCount] = paintingsOnCurrentPlatform;
+            firstPhasePlatformCount++;
+        }
+
+        currentWidth = 0;
+        currentMaxHeight = 0;
+        paintingsOnCurrentPlatform = 0;
+        i = n - 1;
+
+        while (i >= localMin) {
+            if (currentWidth + widths[i] <= w) {
+                if (localMinProcessed && localMin == i) break;
+
+                currentWidth += widths[i];
+                currentMaxHeight = Math.max(currentMaxHeight, heights[i]);
+                paintingsOnCurrentPlatform++;
+
+            } else {
+                totalHeight += currentMaxHeight;
+                numPaintingsOnPlatformSecondPhase[secondPhasePlatformCount] = paintingsOnCurrentPlatform;
+                secondPhasePlatformCount++;
+
+                currentWidth = widths[i];
+                currentMaxHeight = heights[i];
+                paintingsOnCurrentPlatform = 1;
+            }
+            i--;
+        }
+
+        if (paintingsOnCurrentPlatform > 0) {
+            totalHeight += currentMaxHeight;
+            numPaintingsOnPlatformSecondPhase[secondPhasePlatformCount] = paintingsOnCurrentPlatform;
+            secondPhasePlatformCount++;
+        }
+
+        int totalPlatforms = firstPhasePlatformCount + secondPhasePlatformCount;
+        int[] finalPaintingsOnPlatforms = new int[totalPlatforms];
+
+        System.arraycopy(numPaintingsOnPlatformFirstPhase, 0, finalPaintingsOnPlatforms, 0, firstPhasePlatformCount);
+
+        for (int j = 0; j < secondPhasePlatformCount; j++) {
+            finalPaintingsOnPlatforms[firstPhasePlatformCount + j] = numPaintingsOnPlatformSecondPhase[secondPhasePlatformCount - j - 1];
+        }
+
+        return new Result(totalPlatforms, totalHeight, finalPaintingsOnPlatforms);
     }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
